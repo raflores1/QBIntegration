14e03d46cd0beda27c626d3bd091f249cf2c2710
Rafael Flores <rafael.flores@ensobusiness.com>
2017-04-13 22:28:11 -0400

    Configure connection for QBO sandbox."

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -61,6 +61,21 @@ class SuppliersController < ApplicationController
     end
   end
 
+  def authenticate
+    callback = quickbooks_oauth_callback_url
+    token = QB_OAUTH_CONSUMER.get_request_token(:oauth_callback => callback)
+    session[:qb_request_token] = token
+    redirect_to("https://appcenter.intuit.com/Connect/Begin?oauth_token=#{token.token}") and return
+  end
+
+def oauth_callback
+	at = session[:qb_request_token].get_access_token(:oauth_verifier => params[:oauth_verifier])
+	token = at.token
+	secret = at.secret
+	realm_id = params['realmId']
+	# store the token, secret & RealmID somewhere for this user, you will need all 3 to work with Quickbooks-Ruby
+end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_supplier
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :suppliers
+  resources :suppliers do 
+    collection do 
+     get 'authenticate'
+     get 'oauth_callback'
+    end
+  end
+
+root 'suppliers#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
